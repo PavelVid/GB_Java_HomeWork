ac8c9e313b176e72d73edb059fa2a08ec9f2aca0
Pavel <pavelvid@mail.ru>
2023-04-26 08:07:11 +0300

    Seminar_3

---
+++ Seminar_3/Task_2.java
@@ -0,0 +1,19 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+import java.util.ArrayList;
+public class Task_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = lib.FillRandomArray(10);
+        lib.PrintArray(array);
+        DeleteEvenNumber(array);
+        lib.PrintArray(array);
+    }
+   public static void DeleteEvenNumber(ArrayList<Integer> list){
+        int i = 0;
+        while (i < list.size()){
+            if(list.get(i) % 2 == 0) list.remove(i);
+            else i++;
+        }
+    }
+}
